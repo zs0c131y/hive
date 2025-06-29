6240f23b4d4307fa971f864dc11d22b424e97865
Adarsh <108751847+zs0c131y@users.noreply.github.com>
2025-06-29 11:22:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // This makes all paths relative
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
   server: {
     hmr: true,
   },
